1be3c8500d289c3ea469649dcde2a151a198bd54
Monica <monicsbramos@gmail.com>
2024-05-19 10:26:50 +0800

    [ADD -chat support]

--- script.js
+++ script.js
@@ -27,6 +27,18 @@ document.addEventListener('DOMContentLoaded', function() {
         window.location.href = 'dashboard.html';
     });
 });
+const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
+
+allSideMenu.forEach(item=> {
+	const li = item.parentElement;
+
+	item.addEventListener('click', function () {
+		allSideMenu.forEach(i=> {
+			i.parentElement.classList.remove('active');
+		})
+		li.classList.add('active');
+	})
+});
 
 // Function to validate email format
 function validateEmail(email) {
